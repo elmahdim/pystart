101321b0d06a0a3d68dbbab54b0c5ebfcf901caa
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2017-12-28 23:41:09 +0300

    Add numeric types, with supported operations

---
+++ numeric_types.py
@@ -0,0 +1,20 @@
+x = 6
+y = 1.6
+
+print("=======")
+print("x =", x)
+print("y =", y)
+print("=======")
+
+print("⇒ sum of x + y:", x + y)
+print("⇒ difference of x - y:", x - y)
+print("⇒ product of x * y:", x * y)
+print("⇒ quotient of x / y:", x/y)
+print("⇒ (floored) quotient of x // y:", x // y)
+print("⇒ -x, x negated:", -x)
+print("⇒ +x, x unchanged:", +x)
+print("⇒ abs(x), absolute value or magnitude of x:", abs(x))
+print("⇒ int(y), y converted to integer:", int(y))
+print("⇒ float(x), x converted to floating point:", float(x))
+print("⇒ pow(x, 2), x to the power y:", pow(x, 2))
+print("⇒ x ** 2, x to the power y:", x ** 2)
