bb318b643f2eefb8d94ba80425c0f5672e689182
Mahmoud Elmahdi <mahmoud.elmahdi@oxagile.com>
2017-12-29 17:03:50 +0300

    add Strings, String Methods and Slices

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Getting Started with Python Programming Language
 Python Lib Reference | *repl.it*
 --- | ---
 Numeric Types, with supported operations | [run ▸](https://repl.it/@elmahdim/Python-Numeric-Types)
+Strings, String Methods and Slices | [run ▸](https://repl.it/@elmahdim/Python-Strings)
---
+++ strings.py
@@ -0,0 +1,38 @@
+lang = 'python'
+skills = [lang, 'javascript', 'ruby']
+
+print('single quoted')
+print("double quoted")
+print("""Multi
+  Lines.""")
+print('Hello from, ' + 'the other side!')
+
+# String Methods
+
+print(lang.lower())
+print(lang.upper())
+print(lang.capitalize())
+print(' python '.strip())
+print(lang.isalpha())
+print(lang.isdigit())
+print(lang.isspace())
+print(lang.startswith('py'))
+print(lang.endswith('on'))
+print(lang.find('th'))
+print(lang.replace('thon', ''))
+print(lang.split('thon'))
+print(', '.join(skills))
+
+# String Slices
+
+print(lang[0:2])
+print(lang[2:])
+print(lang[:])
+print(lang[-1])
+print(lang[-1:])
+print(lang[:-1])
+
+# String %
+
+print("%d skills: %s and %s and %s" % (3, lang, skills[1], skills[2]))
+
